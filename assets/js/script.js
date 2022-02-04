cef6c43f39fa6e98d8697dff51ad22f6f3c938d6
Jim Nguyen <95522881+jimbn@users.noreply.github.com>
2022-02-03 21:59:07 -0800

    add lat/lon of current location and set geoApifyUrl

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,8 +5,34 @@
 // filter=rect:-122.33530492248485,37.86249960537557,-121.84349507751426,37.484645227626935 is the geo location based on coordinates
 // limit=20 is search limit
 
-const geoApifyUrl = "https://api.geoapify.com/v2/places?categories=pet.dog_park&" + location + "limit=" + limitNum "&apiKey=d44ff70a85d74358b285655b81aa219b";
+const limitNum = 20;
+
+// let inputParameter = [pet.shop, pet.veterinary, pet.service, pet.dog_park];
+const parameter = "pet.shop";
+// radius is a search perameter in meters
+let radius = 100000;
+const lon = localStorage.getItem("lon");
+const lat = localStorage.getItem("lat");
+
+//  if search multiple parameter, must have & between each
+const geoApifyUrl = "https://api.geoapify.com/v2/places?categories=" + parameter + "&filter=circle:" + lon + "," + lat + "," + radius + "&limit=" + limitNum  + "&apiKey=d44ff70a85d74358b285655b81aa219b";
 
 function getGeoApi () {
     fetch(geoApifyUrl);
 }
+
+navigator.geolocation.getCurrentPosition(getLatLon);
+
+function getLatLon(position) {
+
+    var latitude = position.coords.latitude;
+    var longitude = position.coords.longitude;
+        console.log("Latitude is "+latitude);
+        console.log("Longitude is "+longitude);
+
+    localStorage.setItem("lat", latitude);
+    localStorage.setItem("lon", longitude);
+}
+
+
+getGeoApi ();
