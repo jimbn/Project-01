d4f7e399e00a62735f684dc5f7db900dbd48c22c
Jim Nguyen <95522881+jimbn@users.noreply.github.com>
2022-02-08 16:43:50 -0800

    new search result will replace old, instead of apending to bottom

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,23 +2,35 @@
 //  if search multiple parameter, must have & between each
 
 const searchedList = document.getElementById(`listItems`);
+let itemName;
+let searchItem;
 const fetchButton = document.getElementById(`fetchButton`);
+const clearButton = document.getElementById(`clear`);
+const closeModalBtn = document.getElementById(`closeModal`);
 
 let searchedCity;
 let limitNum;
 let radius;
 let selectedParameter;
+let lat;
+let lon;
 const apiKey = "&appid=5bbb7a356faba6df28c3a3229103f17a";
 
-
+// clearButton.addEventListener(`click`, rmvSearchList)
 
 fetchButton.addEventListener(`click`, function() {
-    inputSearchedCity();
-    parameterChecked();
-    selectRadius();
-    selectSearchNumb();
-    getGeoApify ();
+        rmvSearchList();
+        inputSearchedCity();
+        parameterChecked();
+        selectRadius();
+        selectSearchNumb();
+        getGeoApify ();
+    
 });
+
+function rmvSearchList() {
+    searchedList.innerHTML = '';
+}
 
 function inputSearchedCity() {
     searchedCity = document.getElementById(`searchedCity`).value;
@@ -30,7 +42,7 @@ function parameterChecked() {
     let para2 = document.getElementById(`para2`).checked;
     let para3 = document.getElementById(`para3`).checked;
     let para4 = document.getElementById(`para4`).checked;
-    const parameter = [];
+    let parameter = [];
 
     if( para1===true ){
         parameter.push(document.getElementById(`para1`).value);
@@ -46,60 +58,76 @@ function parameterChecked() {
     } 
 
     selectedParameter = parameter.join("&");
+    parameter = []
     console.log(selectedParameter);
+    console.log(parameter);
 }
 
 function selectRadius () {
     radius = document.getElementById(`searchDist`).value;
-    console.log(radius);
+
 }
 
 function selectSearchNumb () {
     limitNum = document.getElementById(`searchNumb`).value;
-    console.log(limitNum);
+
 }
+
 
 
 // function to print list
 function getGeoApify () {
-    let locUrl = "//api.openweathermap.org/geo/1.0/direct?q=" + searchedCity + apiKey;
-    
+    let locUrl = "https://api.openweathermap.org/geo/1.0/direct?q=" + searchedCity + apiKey;
+    console.log(locUrl);
+
+
     fetch(locUrl)
         .then(function(response){
             return response.json();
+
         })
         .then(function(data){
-            const lat = data[0].lat;
-            const lon = data[0].lon;
-            const geoApifyUrl = "//api.geoapify.com/v2/places?categories=" + selectedParameter + "&filter=circle:" + lon + "," + lat + "," + radius + "&limit=" + limitNum  + "&apiKey=d44ff70a85d74358b285655b81aa219b";
-            console.log(geoApifyUrl);
-            fetch(geoApifyUrl)
-                .then(function(response){
-                    return response.json()
+            
+            if(data === []) {
+                modal.style.display = "block";
+            }
+            else {  
+                lat = data[0].lat;
+                lon = data[0].lon;         
+                let geoApifyUrl = "https://api.geoapify.com/v2/places?categories=" + selectedParameter + "&filter=circle:" + lon + "," + lat + "," + radius + "&limit=" + limitNum  + "&apiKey=d44ff70a85d74358b285655b81aa219b";
+ ;
+                fetch(geoApifyUrl)
+                    .then(function(response){
+                        return response.json()
+                    })
+                    .then(function(data){
+                        for(let i=0; i<data.features.length; i++){
+
+                            let searchItem = document.createElement(`li`);
+                            let itemName = document.createElement(`span`);
+                            let itemDetails = document.createElement(`ul`);
+                            let itemAddress = document.createElement(`li`);
+                            
+                            searchedList.append(searchItem);
+                            searchItem.append(itemName);
+                            searchItem.append(itemDetails);
+                            itemDetails.append(itemAddress);
+                            console.log(searchItem);
+                            console.log(itemDetails);
+                            itemName.textContent = data.features[i].properties.address_line1;
+                            itemAddress.textContent = data.features[i].properties.address_line2;
+                        }
+
+                    document.getElementById(`itemsContainer`).className = `bg-info shadow card scroll`;   
                 })
-                .then(function(data){
-                    
+            }
 
-                    for(let i=0; i<data.features.length; i++){
-
-                        let searchItem = document.createElement(`li`);
-                        let itemName = document.createElement(`span`);
-                        let itemDetails = document.createElement(`ul`);
-                        let itemAddress = document.createElement(`li`);
-                        
-                        searchedList.append(searchItem);
-                        searchItem.append(itemName);
-                        searchItem.append(itemDetails);
-                        itemDetails.append(itemAddress);
-                        console.log(searchItem);
-                        console.log(itemDetails);
-
-                        itemName.textContent = data.features[i].properties.address_line1;
-                        itemAddress.textContent = data.features[i].properties.address_line2;
-                    }
-
-                document.getElementById(`itemsContainer`).className = `bg-info shadow card scroll`;   
-                })
         })
 }
 
+
+
+// eventlistner to close modal
+closeModalBtn.addEventListener(`click`,function() {
+    modal.style.display ="none";
+})
